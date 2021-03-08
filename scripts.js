41181bbd757f26e32fb6fd18c0ef4e6e79496c60
jozsef1491 <79862635+jozsef1491@users.noreply.github.com>
2021-03-08 10:05:43 +0100

    add files

---
+++ scripts.js
@@ -0,0 +1,148 @@
+let data={
+    photo: `images/pillango.jpg`,
+    title: `My title`,
+    description: `What happened here, why is this a very nice image`
+}
+
+
+
+let array=[
+    {"photo": `images/pillango.jpg`,
+    "title":`Éjjeli pillangók egy ázsiai erdőben`,
+    "description":`Éjjeli lepkék, bagolylepkék (Noctuina), az izeltlábuak rovarok (Insecta) osztályának,
+a pikkelyesszárnyuak (Lepidoptera) rendjébe tartozó nagylepkék (Macrolepidoptera) egyik nagy csoportja.
+ Zömök, középnagyságu testök többnyire sötétszinezetü, vastag, kupalaku, a himek és a nőstények csápjai rendesen különbözők, 
+ széles szárnyaikat nyugváskor házfedélalakuan v. vizszintesen tartják. Mellső szárnyaik harántsávokkal diszítettek, melyek közül egy fél a szárny tövén, 
+ kettő a középen és egy többnyire W-alaku a szárny szélén található. Ezeken kivül rajtok még határozott alaku foltok, u. m. nyil-, vese-, gyürü- és cseppfolt találhatók. 
+ A sávok és foltok alakja, nagysága és szerkezete igen fontos, mert nemi és faji megkülönböztető jellegül is szolgálhatnak. 
+ A hátsó szárnyak zárókészülékkel vannak ellátva. Legtöbb faj csak éjjel röpül, nappal fahasadékokban, kerítésen, földön, leveleken stb. helyen nyugszanak. 
+ Nevezetes, hogy tartózkodási helyüket mindig ugy választják meg, hogy a környezet szine testök szinezetéhez hasonlítson; 
+ azért gyakran még a leggyakorlottabb szem is alig képes észrevenni.`,
+},
+    {"photo": `images/deer.jpg`,
+    "title":`Erdei szarvas kiér a tisztásra`,
+    "description":`A szarvas a párosujjú patások közé tartozó emlősállat. A szarvasok legtöbb faja az őzekkel együtt a szarvasfélék családjába tartozik. 
+    Egyes nyelveken egyazon szó jelöli a szarvast és az őzet is. (Például az angol deer szó a jelzőjétől függően – red deer, roe deer – jelenthet szarvast vagy őzet.)
+    A szarvasmarhához hasonlóan a szarvas hímjét is bikának, nőstényét tehénnek, kicsinyét pedig borjúnak nevezzük. A szarvasbika legfeltűnőbb jellegzetessége az agancsa. 
+    A rénszarvasnál a tehénnek is van agancsa, a többi szarvasfajnál csak a bikának. Összetett gyomorral rendelkeznek, párosujjú patások, elevenszülők, emlőből szoptatnak. 
+    Általában 1-2 borjút nemzenek.`,
+
+    },
+
+    {"photo": `images/greece.jpg`,
+    "title":`Az athéni Akropolisz`,
+    "description":`Az athéni Akropolisz Görögországban található, az ókorból származó épületegyüttes, az európai civilizáció és demokrácia jelképe, az emberi alkotógéniusz egyik 
+    legnagyobb alkotása. A földrengéseknek kitett területen megépítéséhez mai szemmel is jelentős mérnöki tudásra volt szükség.[1] Az épület számos olyan technikai megoldást rejt, 
+    mely évszázadokra feledésbe merült. Ezeknek köszönhető, hogy ma is áll. Egyike azon kevés világörökségi helyszíneknek, melyek öt kritériumnak is eleget tesznek. 
+    Görögország-szerte számos hasonló akropolisz található, de méretei és kivitelezése miatt az athéni kitüntetett helyet foglal el közöttük.`,
+
+    },
+
+    {"photo": `images/korea.jpg`,
+    "title":`Éjszakai életkép egy koreai kisvárosban`,
+    "description":`Korea (hangul: 한국, átírva: Hanguk; vagy 조선, magyarosan: Csoszon, angolosan: Joseon) Kelet-Ázsia egyik területe, ami 1948 óta megosztott Dél- és 
+    Észak-Korea között. Korea területe a Koreai-félszigeten található, északról Kína és Oroszország, keleten a Koreai-szoros és a Japán-tenger által elválasztva Japán, 
+    és délre a Kelet-kínai-tengeren túl Tajvan határolja. Koreára erős kulturális hatást gyakorolt Kína, melynek írását a Kr. e. 2. században vették át (handzsa), 
+    és a Kr. u. 4. században a buddhizmus gyakorolt erős hatást a koreai három királyságra, amiket először Silla uralkodója, Munmu egyesített.
+     A mongol állam összeomlása után komoly belpolitikai küzdelem bontakozott ki, aminek hatására 1388-ban a Csoszon-dinasztia került hatalomra.
+    Napjainkban Észak-Korea (hivatalosan Koreai Népi Demokratikus Köztársaság) a Kim Ir Szen által kidolgozott dzsucse-rendszert megvalósító pártállami diktatúra, 
+    addig Dél-Korea (hivatalosan Koreai Köztársaság) demokratikus, kapitalista állam, az ázsiai tigrisek egyike.`,
+
+    },
+
+    {"photo": `images/nationalpark.jpg`,
+    "title":`Minden egyes virágban ott a tavasz`,
+    "description":`A nemzeti park a Természetvédelmi Világszövetség meghatározása szerint olyan terület, amelynek ökológiai egységessége megőrzendő a 
+    jelen és a jövő generációi számára; amely megvédendő mindenfajta mezőgazdasági és ipari hasznosítástól; és amelyen lehetőség nyílik nemcsak tudományos, 
+    hanem oktatási és szabadidős tevékenységek végzésére is. A fenti elvek alapján első alkalommal 1864-ben az Amerikai Egyesült Államokban helyeztek állami védelem 
+    alá egy területet. 1864. június 30-án az Abraham Lincoln által aláírt törvény a kaliforniai Yosemite Valley-t és a Giant Sequoias-i Mariposa Grove-ot állami tulajdonba
+     vette, létrehozva ezzel a Yosemite Nemzeti Parkot. Az első valódi nemzeti park, amely létrehozatalának pillanatában a nemzeti park nevet is megkapta, az 1872-ben létrehozott 
+     Yellowstone Nemzeti Park.`,
+
+    },
+
+    {"photo": `images/repulo.jpg`,
+    "title":`Az ember az ókor óta repülni vágyott`,
+    "description":`A repülőgép a levegőnél nehezebb olyan közlekedési eszköz, mely a légkörben halad, merev felületei és a levegő reakcióerejéből keletkező felhajtóerő 
+    segítségével a repülési magasság és irány megváltoztatására, illetve megtartására képes motor vagy hajtómű segítségével. 
+    A motor nélküli merev szárnyú repülőgépek (vitorlázó repülőgép) esetében a magasság megtartása vagy növelése csak emelkedő légáramlat 
+    (termik, vagy ún. lejtőszél) segítségével lehetséges, de az ilyen járművek ennek hiányában is képesek a kontrollált repülésre és jelentős távolság megtételére, 
+    relatíve csekély magasságvesztéssel (1:25 – 1:50 értékű siklószámmal), ezért szintén repülőgépeknek tekintjük őket.`,
+
+    },
+
+    {"photo": `images/salata.jpg`,
+    "title":`Van-e eredeti cézár saláta recept?`,
+    "description":`Ha az ember bármerre keres az interneten, akkor angol nyelven mindenhol egyetlen történetet talál, ami leírja, honnan is származik az eredeti cézár saláta. 
+    Eszerint a saláta megalkotója az Olaszországból az USA-ba emigrált Caesar Cardini, aki ugyan az amerikai San Diegoban élt, de ezt a salátát a határ túloldalán lévő, 
+    mexikói Tijuanaban alkotta meg, ahol az akkoriban érvényben lévő alkoholtilalom miatt üzemeltetett éttermet. A történet szerint ennek az étteremnek a fő célcsoportja a 
+    határon italozni átjáró amerikaiak voltak – San Diego és Tijuana szó szerint csak néhány kilométerre vannak egymástól.`,
+
+    },
+
+    {"photo": `images/woman.jpg`,
+    "title":`A nők helyzete Indiában`,
+    "description":`A szexuális erőszak és zaklatás ugyanazon veszélyeivel kell szembenézniük az indiai nőknek, legyenek nagyvállalatok vezetői, dolgozzanak háztartási 
+    alkalmazottként a középosztály otthonaiban, gyárakban, mezőgazdaságban. 2005 óta legkevesebb 20 millió nő – ez New York, Párizs és London lakossága együttvéve – hagyta 
+    el Ázsia harmadik legnagyobb gazdaságának munkaerőpiacát a Világbank adatai szerint, részben azért, mert rossz bánásmódban részesült munkahelyén. Manapság már csak az 
+    indiai nők 27 százaléka dolgozik hivatalosan. A magánszektorban dolgozók a tömegközlekedés helyett inkább a vállalatok által fizetett taxikon érkeznek munkába, 
+    és sokan nem vesznek részt a cég által szervezett esti rendezvényeken vagy találkozókon biztonsági okok miatt. "Szükségünk van biztonságos közlekedésre és zéró 
+    toleranciára a munkahelyi szexuális zaklatást illetően. Úgy kell nevelnünk fiainkat, hogy tiszteljék a lányokat és a nőket, és nyilvánvalóvá kell tenni, hogy semmiképp 
+    nem tűrjük a nemi alapú erőszakcselekményeket."`,
+
+    },
+]
+
+console.log(array[0].title)
+
+let currentPhoto=0;
+$(`#photo`).attr(`src`, array[currentPhoto].photo)
+
+function kepAdatokBetoltese(x){
+    $(`#photo`).attr(`src`, array[x].photo)
+    $(`.photo-title`).text(`${array[x].title}`)
+    $(`.photo-description`).text(`${array[x].description}`)
+}
+
+$('#jobbranyil').click(function fuggvenynev(){
+    currentPhoto++
+    if(currentPhoto==array.length){
+        currentPhoto=0
+    }
+    kepAdatokBetoltese(currentPhoto)
+})
+
+$('#balranyil').click(function fuggvenynev(){
+    currentPhoto--
+     if(currentPhoto<0){
+        currentPhoto=7
+    }
+    kepAdatokBetoltese(currentPhoto)
+})
+
+for(let i=0;i<array.length;i++){
+    $('.thumbnails').append(`<img class="thumb" src="${array[i].photo}" data-index="${i}">`)
+}
+
+$(`.thumb`).click((event) => {
+    let indexClicked = $(event.target).attr('data-index')
+    let numberIndex = parseInt(indexClicked)
+    currentPhoto=numberIndex
+    kepAdatokBetoltese(currentPhoto)
+    //$(event.target).css('border-color', 'red')
+})
+
+
+/*valamiért működik
+$('.thumb').click(function fuggvenynev(){
+    let index=getAttribute(thumb.this)
+    currentPhoto=numberIndex
+
+})
+
+*/
+
+
+
+
+ 
